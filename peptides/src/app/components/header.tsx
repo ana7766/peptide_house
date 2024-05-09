55e770215b8a1191668216fb8d9ae43b05e3c269
ana7766 <95316333+ana7766@users.noreply.github.com>
2024-05-09 19:02:48 +0200

    Fix

--- peptides/src/app/components/header.tsx
+++ peptides/src/app/components/header.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+"use client";
+import React, { useState } from 'react';
 import Image from 'next/image';
 
 function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div className="flex flex-col items-center justify-between p-14">
-      <nav className="max-w-5xl w-full mx-auto flex justify-between items-center">
+      <nav className="max-w-5xl w-full mx-auto flex justify-between items-center hidden md:flex">
         <div>
           <Image src="/logo.png" alt="Logo" width={309} height={37} />
         </div>
@@ -31,6 +38,50 @@ function Header() {
           </li>
         </ul>
       </nav>
+
+      <div className="md:hidden">
+        <div className="flex justify-between items-center w-full">
+          <div className='mr-20'>
+            <Image src="/logo.png" alt="Logo" width={309} height={37} />
+          </div>
+          <button
+            className="block md:hidden"
+            onClick={toggleMenu}
+            aria-label="Toggle Menu"
+          >
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16m-7 6h7"
+              ></path>
+            </svg>
+          </button>
+        </div>
+        {isMenuOpen && (
+          <div className="w-full flex flex-col items-center">
+            <a href="#" className="text-gray-800 my-2">
+              What Are Peptides
+            </a>
+            <a href="#" className="text-gray-800 my-2">
+              Peptides for Muscle Grow
+            </a>
+            <a href="#" className="text-gray-800 my-2">
+              Muscle Grow
+            </a>
+            <a href="#" className="text-gray-800 my-2">
+              FAQ&apos;s
+            </a>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
