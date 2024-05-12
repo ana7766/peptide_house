e07b2de2c5059055f51e1e1cb4083a47267011c9
ana7766 <95316333+ana7766@users.noreply.github.com>
2024-05-12 12:31:01 +0200

    Fix card

--- peptides/src/app/components/card.tsx
+++ peptides/src/app/components/card.tsx
@@ -18,10 +18,10 @@ const Card: React.FC<CardProps> = ({ mainText, additionalText, index }) => {
 
   return (
     <div className="container mx-auto items-center p-4 max-w-5xl w-full">
-      <div className={`border border-solid border-gray-300 rounded-lg relative min-h-[47px]`}>
-        <div className="m-2 flex justify-between items-center">
-         <div className='text max-[530px]: text flex-1 font-bold'> {mainText}</div>
-          <button className="text-blue-500 cursor-pointer items-center" onClick={toggleCard}>
+      <div className={`relative min-h-[47px]`}>
+        <div className="flex cursor-pointer items-center rounded-[5px] border-[1px] border-solid border-gray-300 px-[14px] py-[11px] transition duration-300 ease-linear selection:bg-slate-500 hover:border-blue-400 hover:bg-blue-200 sm:w-[100%] sm:px-[16px] sm:py-[13px] md:w-[100%]">
+         <div className='text max-[530px] flex-1 font-bold'> {mainText}</div>
+          <button className="text-blue-500 cursor-pointer items-center flex h-[12px] w-[12px]" onClick={toggleCard}>
             {isOpen ? (
               <svg
               width="12"
